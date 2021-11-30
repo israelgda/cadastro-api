fc41914ab4dd09a15367b9c101486520472f9080
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-30 19:39:27 -0300

    Feat: Criado o verificador de data

---
+++ src/main/java/com/israelgda/cadastroapi/utils/VerificadorData.java
@@ -0,0 +1,26 @@
+package com.israelgda.cadastroapi.utils;
+
+import com.israelgda.cadastroapi.services.exceptions.BirthDateInvalidFormatException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class VerificadorData {
+
+    public static String verificaData(String data){
+        String dateFormat = "dd/MM/uuuu";
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+                .ofPattern(dateFormat)
+                .withResolverStyle(ResolverStyle.STRICT);
+        try {
+            LocalDate date = LocalDate.parse(data, dateTimeFormatter);
+            return data;
+        } catch (DateTimeParseException e) {
+            throw new BirthDateInvalidFormatException("");
+        }
+    }
+
+}

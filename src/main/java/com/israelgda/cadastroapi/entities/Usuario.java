6c30384fedcd95caf258e36d7a847ef230511b5f
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-30 16:24:02 -0300

    Feat: Entidade Usuario mapeada

--- src/main/java/com/israelgda/cadastroapi/entities/Usuario.java
+++ src/main/java/com/israelgda/cadastroapi/entities/Usuario.java
@@ -4,18 +4,41 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name="tb_usuarios")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @NotNull
+    @Column(nullable = false)
     private String nome;
+
+    @NotNull
+    @Column(name= "datanascimento", nullable = false)
+    @Size(min = 10, max = 10)
     private String dataNascimento;
+
     private String cidade;
     private String bairro;
     private String estado;
+
+    @NotNull
+    @Column(unique = true)
+    @Size(min = 11, max = 11)
     private String cpf;
+
+    @Size(min = 11, max = 11)
     private String telefone;
 
     public Usuario(){

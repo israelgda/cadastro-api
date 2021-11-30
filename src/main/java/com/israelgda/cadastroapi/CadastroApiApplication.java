d9ddf6df6093057fc6f7d68e9f83b9b18a0ca042
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-30 14:22:52 -0300

    Commit inicial

---
+++ src/main/java/com/israelgda/cadastroapi/CadastroApiApplication.java
@@ -0,0 +1,13 @@
+package com.israelgda.cadastroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelgda/cadastroapi/CadastroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelgda.cadastroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
